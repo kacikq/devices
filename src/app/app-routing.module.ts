ba6fd105b8dd65195667920ac88e54a61ce76125
Kastriot Quni <e11739847@student.tuwien.ac.at>
2023-06-28 17:34:23 +0200

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ConnectedDevicesComponent} from "./components/connected-devices/connected-devices.component";
+import {DevicesComponent} from "./components/devices/devices.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'connected-devices', component: ConnectedDevicesComponent},
+  {path: 'devices', component: DevicesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnectedDevicesComponent } from './components/connected-devices/connected-devices.component';
+import { DevicesComponent } from './components/devices/devices.component';
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectedDevicesComponent,
+    DevicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    RouterModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [AppRoutingModule]
 })
 export class AppModule { }
